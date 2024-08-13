b3119f90e669b449f82adaa33440ebc32866fec6
HengyueL <lianghengyue1993@outlook.com>
2024-08-13 14:18:14 -0500

    Update

--- debug.py
+++ debug.py
@@ -1,12 +1,23 @@
 import os
 from PIL import Image
 import numpy as np
-
+import pickle
 
 if __name__ == "__main__":
-    img_dir = os.path.join(
-        "dataset", "Clean", "COCO", "Img-1.png"
+    # === Test PIL image format after reading ===
+    # img_dir = os.path.join(
+    #     "dataset", "Clean", "COCO", "Img-1.png"
+    # )
+    # img_pil = Image.open(img_dir).convert('RGB')
+    # img_np = np.asarray(img_pil)
+    # print()
+    
+
+    # === Test if decode result is correct ===
+    test_dir = os.path.join(
+        "Result-Decoded", "Rosteals", "COCO", 
+        "vae", "cheng2020-anchor", "Img-1.pkl"
     )
-    img_pil = Image.open(img_dir).convert('RGB')
-    img_np = np.asarray(img_pil)
+    with open(test_dir, 'rb') as handle:
+        data_dict = pickle.load(handle)
     print()
