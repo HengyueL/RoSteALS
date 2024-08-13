a8295a3d67a217f20fbb0874c18205f3e3d85588
HengyueL <lianghengyue1993@outlook.com>
2024-08-13 17:15:04 -0500

    Update

--- collect_sample.sh
+++ collect_sample.sh
@@ -1,2 +1,4 @@
-python collect_dataset.py --dataset_name COCO &> logs/COCO.txt
-python collect_dataset.py --dataset_name DiffusionDB &> logs/DiffusionDB.txt
+# python collect_dataset.py --dataset_name COCO &> logs/COCO.txt
+# python collect_dataset.py --dataset_name DiffusionDB &> logs/DiffusionDB.txt
+
+python decode_interm_result.py
---
+++ decode_interm.sh
@@ -0,0 +1,2 @@
+python decode_interm_result.py --dataset COCO
+python decode_interm_result.py --dataset DiffusionDB
