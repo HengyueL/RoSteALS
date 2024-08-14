c791b2fa7ba454f59c42f51ab14aeeb613caca7d
HengyueL <lianghengyue1993@outlook.com>
2024-08-14 13:54:34 -0500

    Update

--- decode_interm.sh
+++ decode_interm.sh
@@ -1,2 +1,7 @@
-python decode_interm_result.py --dataset COCO
-python decode_interm_result.py --dataset DiffusionDB
+python decode_interm_result.py --dataset DiffusionDB --evade_method corrupters --arch contrast
+python decode_interm_result.py --dataset DiffusionDB --evade_method corrupters --arch gaussian_noise
+python decode_interm_result.py --dataset DiffusionDB --evade_method corrupters --arch jpeg
+python decode_interm_result.py --dataset DiffusionDB --evade_method diffpure --arch dummy
+python decode_interm_result.py --dataset DiffusionDB --evade_method diffuser --arch dummy
+python decode_interm_result.py --dataset DiffusionDB --evade_method dip --arch vanila
+python decode_interm_result.py --dataset DiffusionDB --evade_method vae --arch cheng2020-anchor
